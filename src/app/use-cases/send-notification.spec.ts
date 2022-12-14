94a11943dcda4bd43f8612900ef0526c51a7f209
Guilherme Abreu <gglabreu@hotmail.com>
2022-12-14 20:53:03 -0300

    feat: notifications repository

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,15 +1,26 @@
-import { SendNotification } from "./send-notification"
+import { Notification } from '../entities/notification';
+import { SendNotification } from './send-notification';
 
-describe('Send notification', () =>  {
-    it('should be able to send a notification', async () => {
-        const sendNotification = new SendNotification()
+const notifications: Notification[] = [];
 
-        const { notification } = await sendNotification.execute({
-            content: 'This is a notification',
-            category: 'social',
-            recipientId: 'example-recipient-id',
-        })
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
-        expect(notification).toBeTruthy()
-    })
-})
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    console.log(notifications)
+
+    expect(notifications).toHaveLength(1);
+  });
+});
