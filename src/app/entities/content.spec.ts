b48e6901b407da984efb42e8937e05161adaa969
Guilherme Abreu <gglabreu@hotmail.com>
2022-12-14 19:54:43 -0300

    feat: content test

---
+++ src/app/entities/content.spec.ts
@@ -0,0 +1,17 @@
+import { Content } from "./content"
+
+test ('it should be able to create a notification content', () => {
+    const content = new Content('Você recebeu uma nova mensagem!')
+
+    expect(content).toBeTruthy()
+})
+
+test ('it not should be able to create a notification content with less than 5 characters', () => {
+
+    expect(() => new Content('Olá')).toThrow()
+})
+
+test ('it not should be able to create a notification content with more than 240 characters', () => {
+
+    expect(() => new Content('a'.repeat(241))).toThrow()
+})
