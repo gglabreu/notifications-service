04b95defd7c0b8003fd1a768524436f6ac1ae447
Guilherme Abreu <gglabreu@hotmail.com>
2022-12-14 21:50:27 -0300

    feat: new architecture

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,5 +1,5 @@
 import { Notification } from 'src/app/entities/notification';
-import { NotificationsRepository } from 'src/app/repositories/notifications-repositories';
+import { NotificationsRepository } from 'src/app/repositories/notifications-repository';
 
 export class InMemoryNotificationsRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
