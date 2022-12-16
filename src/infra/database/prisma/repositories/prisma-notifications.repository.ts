abdb112bfe7b819522a5d4590fbb82df417029b7
Guilherme Abreu <gglabreu@hotmail.com>
2022-12-15 21:26:18 -0300

    fix: injectable on prisma-notifications.repository

--- src/infra/database/prisma/repositories/prisma-notifications.repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../prisma.service';
 import { Notification } from 'src/app/entities/notification';
 import { NotificationsRepository } from '../../../../app/repositories/notifications-repository';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
     constructor(
         private prismaService: PrismaService
