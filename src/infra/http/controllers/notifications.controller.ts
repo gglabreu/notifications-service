32c4d4664dcc448475012ad7e0acb4dcc399329a
Guilherme Abreu <gglabreu@hotmail.com>
2022-12-28 20:11:37 -0300

    feat: notification not found error and cancel notification

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,3 +1,4 @@
+import { NotificationViewModel } from './../view-models/notification-view-model';
 import { SendNotification } from './../../../app/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { Body, Controller, Post } from '@nestjs/common';
@@ -16,6 +17,6 @@ export class NotificationsController {
       category,
     });
 
-    return { notification };
+    return { notification: NotificationViewModel.toHTTP(notification) };
   }
 }
